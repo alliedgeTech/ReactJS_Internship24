9816c9f17cb1ed0d11b80571d7a7e31238ee3b32
DipPanchal <panchaldip029@gmail.com>
2024-04-05 11:44:32 +0530

    second commitment

---
+++ src/Maincontent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './style.css'
+export const Maincontent = () => {
+    return (
+        <>
+            <div class="card">
+                <h5 class="card-header" >Featured</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="https://www.alliedge.in" target= "_blank"  class="btn btn-success">Go to Alliedge</a>
+                </div>
+            </div>
+            <div class="card">
+                <h5 class="card-header">Featured</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="https://www.alliedge.in" target= "_blank" class="btn btn-success" >Go to Alliedge</a>
+                </div>
+            </div>
+        </>
+    )
+}
